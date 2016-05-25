1e00b00be0b934803479a5a1be7815c693d5ca32
Vinicius <vini.type@gmail.com>
2016-05-25 15:23:50 -0500

    updating the second test

--- community_crime.py
+++ community_crime.py
@@ -1,4 +1,25 @@
-
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import r2_score
 
-df = pd.read_csv('community_crime.csv',header=0)
+alpha = 0.5
+l_ratio = 1e-4
+
+# Split data in train set and test set
+from sklearn.cross_validation import train_test_split
+
+ds = pd.read_csv('community_crime.csv',header=0)
+X = ds.values # it returns a numpy array
+n_samples, n_features = X.shape
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33,random_state=0
+
+########################################################
+# Work with scikit
+
+########################################################
+from sklearn.linear_model import ElasticNet
+
+enet = ElasticNet(alpha=alpha, l1_ratio=l_ratio)
+
+
