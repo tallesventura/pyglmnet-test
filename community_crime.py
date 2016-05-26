40cdcdd44f8ceba4cfaf99fef594b366efa1e990
Vinicius <vini.type@gmail.com>
2016-05-26 17:22:38 -0500

    scikit and pyglmnet examples - final

--- community_crime.py
+++ community_crime.py
@@ -1,11 +1,17 @@
+"""
+==============================
+Communities and Crime - scikit and pyglmnet
+==============================
+
+This is a real life example demonstrating how pyglmnet with
+poisson exponential distribution works and comparing it against scikit.
+
+"""
+
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import r2_score
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-
-alpha = 0.1
-l_ratio = 1e-4
 
 # Split data in train set and test set
 from sklearn.cross_validation import train_test_split
@@ -17,18 +23,6 @@ n_samples, n_features = X.shape
 X, y = np.array(ds.drop(['att128'],axis=1)), np.array(ds['att128'])
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=0)
-
-########################################################
-# Work with scikit
-
-########################################################
-from sklearn.linear_model import ElasticNet
-
-enet = ElasticNet(alpha=alpha, l1_ratio=l_ratio)
-y_pred_enet = enet.fit(X_train, y_train).predict(X_test)
-r2_score_enet = r2_score(y_test, y_pred_enet)
-# print(enet)
-print("r^2 on test data using sklearn : %f" % r2_score_enet)
 
 ########################################################
 # Work with pyglmnet
@@ -49,6 +43,19 @@ r2_score_glm = glm[-1].score(y_test, y_pred_glm, np.mean(y_train), method='pseud
 # r2_score_glm = r2_score(y_test, y_pred_glm)
 print("r^2 on test data using pyglmnet : %f" % r2_score_glm)
 
+########################################################
+# Work with scikit
+
+########################################################
+from sklearn.linear_model import ElasticNet
+
+lambda_ = glm[-1].reg_lambda
+
+enet = ElasticNet(alpha=0.2, l1_ratio=lambda_)
+y_pred_enet = enet.fit(X_train, y_train).predict(X_test)
+r2_score_enet = r2_score(y_test, y_pred_enet)
+# print(enet)
+print("r^2 on test data using sklearn : %f" % r2_score_enet)
 
 ########################################################
 # Plot ...
@@ -64,11 +71,5 @@ plt.xlabel('')
 plt.ylabel('')
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1,
            ncol=2, borderaxespad=0.)
-
-# plt.plot(enet.coef_, label='Elastic net coefficients')
-# plt.plot(, '--', label='original coefficients')
-# plt.legend(loc='best')
-# plt.title("Elastic Net R^2: %f"
-#           % ( r2_score_enet))
 plt.show()
 
