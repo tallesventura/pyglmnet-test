1dd5edc965df770afb5b9d37ce911508b29fc33d
Vinicius <vini.type@gmail.com>
2016-05-26 14:10:15 -0500

    Functional scikit example

--- community_crime.py
+++ community_crime.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import r2_score
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
-alpha = 0.5
+alpha = 0.1
 l_ratio = 1e-4
 
 # Split data in train set and test set
@@ -24,5 +25,39 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33,random_st
 from sklearn.linear_model import ElasticNet
 
 enet = ElasticNet(alpha=alpha, l1_ratio=l_ratio)
+y_pred_enet = enet.fit(X_train, y_train).predict(X_test)
+r2_score_enet = r2_score(y_test, y_pred_enet)
+# print(enet)
+print("r^2 on test data using sklearn : %f" % r2_score_enet)
 
+########################################################
+# Work with pyglmnet
+
+########################################################
+
+from pyglmnet import GLM
+
+reg_lambda = np.logspace(np.log(0.5), np.log(0.01), 10, base=np.exp(1))
+glm = GLM(distr='poisson', alpha=alpha, reg_lambda=reg_lambda, learning_rate=l_ratio ,verbose=False)
+
+########################################################
+# Plot ...
+
+########################################################
+
+#plotting the predictions
+plt.plot(y_test, label='tr')
+plt.plot(y_pred_enet, 'r', label='sklearn-pr')
+plt.plot(y_pred_pyglm, 'r', label='pyglmnet-pr')
+plt.xlabel('')
+plt.ylabel('')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1,
+           ncol=2, borderaxespad=0.)
+
+# plt.plot(enet.coef_, label='Elastic net coefficients')
+# plt.plot(, '--', label='original coefficients')
+# plt.legend(loc='best')
+# plt.title("Elastic Net R^2: %f"
+#           % ( r2_score_enet))
+plt.show()
 
